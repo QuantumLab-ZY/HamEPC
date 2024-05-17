b00f70766479611929107fc5bcd00781e5568ac2
Yang Zhong <59390411+QuantumLab-ZY@users.noreply.github.com>
2024-05-17 19:38:32 +0800

    Update run_EPC.py

--- HamEPC/run_EPC.py
+++ HamEPC/run_EPC.py
@@ -11,22 +11,21 @@ import yaml
 from mpi4py import MPI
 from .EPC_calculator import EPC_calculator
 
-def main(config_file_name):
+def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     rank_size = comm.Get_size()
-
-    with open(config_file_name, encoding='utf-8') as rstream:
+    
+    parser = argparse.ArgumentParser(description="Run EPC Calculator with a specified configuration file.")
+    parser.add_argument('--config', type=str, required=True, help='Path to the configuration file.')
+    args = parser.parse_args()
+    
+    with open(args.config, encoding='utf-8') as rstream:
         input_data = yaml.load(rstream, yaml.SafeLoader)
 
     epc_cal = EPC_calculator(input_data, comm)
     epc_cal.run()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run EPC Calculator with a specified configuration file.")
-    parser.add_argument('--config', type=str, required=True, help='Path to the configuration file.')
+if __name__ == "__main__":    
+    main()
     
-    args = parser.parse_args()
-    
-    main(args.config)
-
